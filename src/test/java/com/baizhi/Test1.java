95479386a96e572362015f2cacd9a4a5d692508a
wanqing <2435351633@qq.com>
2019-12-31 14:34:22 +0800

    cmfz v1.0

---
+++ src/test/java/com/baizhi/Test1.java
@@ -0,0 +1,21 @@
+package com.baizhi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @ClassName Test1
+ * @Discription
+ * @Author
+ * @Date 2019/12/31 14:31
+ **/
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class Test1 {
+    @Test
+    public void test1() {
+        System.out.println("嘻嘻");
+    }
+}
